ef638cbdbe322ba1c7e6e016d32a2b721d70e1b9
DESKTOP-D5E2JEN\Danil <danil.bel53@gmail.com>
2020-12-04 18:33:42 +0300

    Start use pre compiled headers

--- PCH.h
+++ PCH.h
@@ -2,8 +2,15 @@
 using std::cin;
 using std::cout;
 using std::endl;
+using std::ostream;
+
 
 #include <fstream>
+using std::ifstream;
+using std::ofstream;
+
+#include <sstream>
+using std::istringstream;
 
 #include <string>
 using std::string;
@@ -12,17 +19,26 @@ using std::string;
 using std::deque;
 
 #include <algorithm>
+using std::sort;
+using std::copy_if;
 
 
 #include <iterator>
 using std::front_inserter;
 using std::ostream_iterator;
+using std::begin;
+using std::end;
 
 #include <vector>
 using std::vector;
 
 #include <cassert>
 
-#include <iterator>
-using std::front_inserter;
-using std::ostream_iterator;
+#include <any>
+
+#include <functional>
+
+#include <chrono>
+
+#include <exception>
+#include <stdexcept>
